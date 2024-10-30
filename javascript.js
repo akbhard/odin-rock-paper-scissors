1e639b9ce6d9d2dc379a22a8eeb7d816de39c654
Akshay <149981521+akbhard@users.noreply.github.com>
2024-10-29 23:00:01 -0400

    Code in both html and js files to allow the user to select buttons to play the game

--- javascript.js
+++ javascript.js
@@ -14,32 +14,33 @@ function getComputerChoice() {
 
 // console.log(`Computer Choice: ${getComputerChoice()}`)
 
-function getHumanChoice() {
-    let humanChoice = prompt("Rock, Paper, Scissors?");
+// function getHumanChoice() {
+//     let humanChoice = prompt("Rock, Paper, Scissors?");
 
-    if (humanChoice.toLowerCase() === "rock") {
-        return "Rock"
-    } else if (humanChoice.toLowerCase() === "paper") {
-        return "Paper"
-    } else if (humanChoice.toLowerCase() === "scissors") {
-        return "Scissors"
-    } else {
-        console.log(`Invalid choice. Please choose Rock, Paper, or Scissors.`);
-    }
-}
+//     if (humanChoice.toLowerCase() === "rock") {
+//         return "Rock"
+//     } else if (humanChoice.toLowerCase() === "paper") {
+//         return "Paper"
+//     } else if (humanChoice.toLowerCase() === "scissors") {
+//         return "Scissors"
+//     } else {
+//         console.log(`Invalid choice. Please choose Rock, Paper, or Scissors.`);
+//     }
+// }
+
 
 function playRound(humanChoice, computerChoice) {
     if (humanChoice === computerChoice) {
         computerScore ++
         humanScore ++
         return 'Tie! You both get one point!'
-    } else if (humanChoice === 'Paper' && computerChoice === 'Scissors') {
+    } else if (humanChoice === "Paper" && computerChoice === "Scissors") {
         computerScore ++
         return `You lose! ${computerChoice} beats ${humanChoice}`
-    } else if (humanChoice === 'Rock' && computerChoice === 'Paper') {
+    } else if (humanChoice === "Rock" && computerChoice === "Paper") {
         computerScore ++
         return `You lose! ${computerChoice} beats ${humanChoice}`
-    } else if (humanChoice === 'Scissors' && computerChoice === 'Rock') {
+    } else if (humanChoice === "Scissors" && computerChoice === "Rock") {
         computerScore ++
         return `You lose! ${computerChoice} beats ${humanChoice}`
     } else {
@@ -48,21 +49,58 @@ function playRound(humanChoice, computerChoice) {
     }
 }
 
-function playGame() {
-    while (humanScore < 5 && computerScore < 5) {
-        const humanChoice = getHumanChoice();
-        const computerChoice = getComputerChoice();
+const body = document.querySelector("body")
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        const humanChoice = button.id
+        const computerChoice = getComputerChoice()
         const result = playRound(humanChoice, computerChoice)
-
-        console.log(result)
-        console.log(`Score: Human ${humanScore}, Computer ${computerScore}`)
         
-        if (humanScore === 5) {
-                console.log('Human has won! Their score is 5!')
-            } else if (computerScore === 5) {
-                console.log('Computer has won! Their score is 5!')
-            }
-        }
-    }
+        const humanResults = document.createElement("div");
+        humanResults.classList.add("human")
+        humanResults.textContent = `Human Choice = ${button.id}`
+        humanResults.style.padding = "5px"
 
-playGame();
+        const computerResults = document.createElement("div");
+        computerResults.classList.add("computer")
+        computerResults.textContent = `Computer Choice = ${computerChoice}`
+        computerResults.style.padding = "5px"
+
+        const roundResults = document.createElement("div")
+        roundResults.classList.add("round")
+        roundResults.textContent = `Round Results: ${result}`
+        roundResults.style.padding = "5px"
+
+        const score = document.createElement("div")
+        score.classList.add("score")
+        score.textContent = `Human ${humanScore} Computer ${computerScore}`
+        score.style.padding = "5px"
+
+        body.appendChild(humanResults)
+        body.appendChild(computerResults)
+        body.appendChild(roundResults)
+        body.appendChild(score)
+    })
+})
+
+// function playGame() {
+//     while (humanScore < 5 && computerScore < 5) {
+//         const humanChoice = getHumanChoice();
+//         const computerChoice = getComputerChoice();
+//         const result = playRound(humanChoice, computerChoice)
+
+//         console.log(result)
+//         console.log(`Score: Human ${humanScore}, Computer ${computerScore}`)
+        
+//         if (humanScore === 5) {
+//                 console.log('Human has won! Their score is 5!')
+//             } else if (computerScore === 5) {
+//                 console.log('Computer has won! Their score is 5!')
+//             }
+//         }
+//     }
+
+// playGame();
+
